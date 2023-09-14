87af519b949b1c4895d9f63ffbe3b899606bfbbb
Omikunle Timileyin Micheal <Omikunletimilehin@gmail.com>
2023-09-14 15:44:33 +0100

    comit changes

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,13 +6,16 @@ namespace UwanaTimmyName
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your first name?");
+            Console.WriteLine("What is your first name? ");
             string firstName = Console.ReadLine();
 
-            Console.WriteLine("What is your last name?");
+            Console.WriteLine(firstName);
+
+            Console.WriteLine("What is your last name? ");
             string lastName = Console.ReadLine();
 
-            // Display the formatted name
+            Console.WriteLine(lastName);
+            
             Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
         }
     }
