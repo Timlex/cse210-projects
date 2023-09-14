6f479755d9136bf109d0dce8014d15a84cf1391d
Omikunle Timileyin Micheal <Omikunletimilehin@gmail.com>
2023-09-14 16:17:18 +0100

    changes in code 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,47 @@
 using System;
 
-class Program
+namespace SimpleFunctionsProgram
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void Main(string[] args)
+        {
+            DisplayWelcome();
+
+            string userName = PromptUserName();
+            int userNumber = PromptUserNumber();
+            int squaredNumber = SquareNumber(userNumber);
+
+            DisplayResult(userName, squaredNumber);
+        }
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            return number;
+        }
+
+        static int SquareNumber(int number)
+        {
+            return number * number;
+        }
+
+        static void DisplayResult(string userName, int squaredNumber)
+        {
+            Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
+        }
     }
 }
