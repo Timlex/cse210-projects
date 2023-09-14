376740ab77d9078d0cb1b55a12066c3437eefd6a
Omikunle Timileyin Micheal <Omikunletimilehin@gmail.com>
2023-09-14 16:04:48 +0100

    changes in the code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,56 @@
 using System;
 
-class Program
+namespace GuessMyNumber
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep3 World!");
+        static void Main(string[] args)
+        {
+            bool playAgain = true;
+
+            while (playAgain)
+            {
+                // Generate a random number between 1 and 100 (inclusive)
+                Random random = new Random();
+                int magicNumber = random.Next(1, 101);
+
+                int numberOfGuesses = 0;
+
+                Console.WriteLine("Welcome to the Guess My Number game!");
+                Console.WriteLine("I have selected a magic number between 1 and 100.");
+                Console.WriteLine("Try to guess it!");
+
+                while (true)
+                {
+                    Console.Write("Enter your guess: ");
+                    int userGuess = Convert.ToInt32(Console.ReadLine());
+                    numberOfGuesses++;
+
+                    if (userGuess < magicNumber)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                    else if (userGuess > magicNumber)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You guessed it in {numberOfGuesses} guesses!");
+                        break;
+                    }
+                }
+
+                Console.Write("Do you want to play again? (yes/no): ");
+                string playAgainResponse = Console.ReadLine().ToLower();
+
+                if (playAgainResponse != "yes")
+                {
+                    playAgain = false;
+                }
+            }
+
+            Console.WriteLine("Thanks for playing!");
+        }
     }
 }
